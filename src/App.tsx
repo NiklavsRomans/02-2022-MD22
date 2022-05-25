23f1175675d058202a307827af1fef2c3a1fa10c
Niklavs Romans <niklavsromans19@gmail.com>
2022-05-26 01:29:49 +0200

    main files

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/homepage/HomePage';
+import SinglePokemonPage from './pages/singlepokemonpage/SinglePokemonPage';
+import Header from './components/header/Header';
 
 const App = () => (
   <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit
-        {' '}
-        <code>src/App.tsx</code>
-        {' '}
-        and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/pokemon/:name" element={<SinglePokemonPage />} />
+      </Routes>
+    </Router>
   </div>
 );
 
